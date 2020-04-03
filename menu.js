4a59f918829896af71fadac24a01ec718402584c
chiharu15 <y7531022@eng.kitakyu-u.ac.jp>
2020-04-03 15:46:48 +0900

    アイコン修正　hoverスマホ対応

--- menu.js
+++ menu.js
@@ -22,26 +22,21 @@ $(function() {
   });
 
   var scrollMenu = function() {
-		// 配列宣言
-		// ここにスクロールで点灯させる箇所のidを記述する
-		// 数値は全て0でOK
+
 		var array = {
+			'#profile': 0,
 			'#works': 0,
-			'#about': 0,
 			'#skill': 0
 		};
 
 		var $globalNavi = new Array();
-
-		// 各要素のスクロール値を保存
 		for (var key in array) {
 			if ($(key).offset()) {
-				array[key] = $(key).offset().top - 10; // 数値丁度だとずれるので10px余裕を作る
+				array[key] = $(key).offset().top - 10; 
 				$globalNavi[key] = $('a[href="' + key + '"]');
 			}
 		}
 
-		// スクロールイベントで判定
 		$(window).scroll(function () {
 			for (var key in array) {
 				if ($(window).scrollTop() > array[key] - 50) {
@@ -53,37 +48,7 @@ $(function() {
 			}
 		});
 	}
-
-	// 実行
 	scrollMenu();
-
-	let mouseMoved = false;
-	const button = document.querySelector('.infoButton');
-
-	const mouseMoveHandler = event => {
-    clearInterval(loop);
-    document.onmousemove = null;
-    button.classList.remove('infoButton_isActive');
-};
-
-const toggleHandler = event => {    
-    const classes = button.classList;
-    if (classes.contains('infoButton_isActive')) {
-        classes.remove('infoButton_isActive');
-    } else {
-        classes.add('infoButton_isActive');
-    }
-}
-
-document.onmousemove = mouseMoveHandler;
-
-const loop = setInterval(toggleHandler, 1000);
-
-
-
-$('.btn3').click(function() {
-    $('.trans3').toggleClass('active');
-  });
 
 });
 
